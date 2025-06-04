c769ed57f67391d229093bd20113e8d45db023f7
Ahmet Kilinc <akx9@icloud.com>
2025-06-04 13:00:47 +0100

    update update

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { GithubIcon } from "lucide-react";
+import { ArrowUpRightIcon, GithubIcon } from "lucide-react";
 import { ModeToggle } from "./theme-toggle";
 import { Button } from "./ui/button";
 
@@ -7,8 +7,15 @@ export const AppHeader = () => {
   return (
     <header className="">
       <div className="mx-auto flex max-w-[100rem] flex-row items-center justify-between px-4 py-8">
-        <h1 className="text-3xl font-medium tracking-tight">ui.ahmet.studio</h1>
-        <div>
+        <h1 className="text-xl font-medium tracking-tight md:text-3xl">
+          ui.ahmet.studio
+        </h1>
+        <div className="flex flex-row items-center md:gap-1">
+          <Button variant="ghost" size="sm" asChild>
+            <Link href="https://dub.sh/ahmet">
+              Portfolio <ArrowUpRightIcon className="h-[1.2rem] w-[1.2rem]" />
+            </Link>
+          </Button>
           <Button variant="ghost" size="sm" asChild>
             <Link href="https://l.ahmet.studio/gh">
               <GithubIcon className="h-[1.2rem] w-[1.2rem]" />
